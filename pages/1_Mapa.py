d5ea184977aaaa87b13bcd0bd173c4342c7b070d
ElianaRM <110210138+Darhudda@users.noreply.github.com>
2025-04-12 15:28:39 -0500

    Agregué lectura del CSV y marcadores dinámicos en el mapa

--- pages/1_Mapa.py
+++ pages/1_Mapa.py
@@ -2,16 +2,23 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 
+import pandas as pd
+
+# Cargar el dataset
+df = pd.read_csv("energia_solar.csv")
+
 st.set_page_config(page_title="Mapa", layout="wide")
 
 st.title("Mapa Interactivo con Folium")
 
 m = folium.Map(location=[4.6097, -74.0818], zoom_start=6)
 
-folium.Marker(
-    location=[4.6097, -74.0818],
-    popup="Bogotá",
-    tooltip="Click para más info"
-).add_to(m)
+for i, row in df.iterrows():
+    folium.Marker(
+        location=[row["Latitud"], row["Longitud"]],
+        popup=f"{row['Departamento']}, {row['Panel_ID']}",
+        tooltip=f"Producción: {row['Producción_kWh']} kWh"
+    ).add_to(m)
+
 
 st_folium(m, width=700, height=500)
